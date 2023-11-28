7f167beb7fc055ba000546dd54316c3b5b5d764a
Alexander Jones <alexander.jones@corndel.com>
2023-11-28 10:58:42 +0000

    Add image caching

--- chessington/ui/static/scripts/image_loader.js
+++ chessington/ui/static/scripts/image_loader.js
@@ -1,9 +1,24 @@
+const imageCache = {}
+
+export async function buildImageCache() {
+    const pieces = ["pawn", "rook", "knight", "bishop", "king", "queen"];
+    for (const piece of pieces) {
+        await loadPieceImage(piece, "white")
+        await loadPieceImage(piece, "black")
+    }
+}
+
 export async function loadPieceImage(piece, player) {
     const imageFileName = `${capitaliseFirstLetter(piece)}${capitaliseFirstLetter(player)}.svg`
+    if (imageCache[imageFileName]) {
+        return imageCache[imageFileName]
+    }
+
     const imageLocation = `/static/images/${imageFileName}`
 
     const response = await fetch(imageLocation)
     const svgData = await response.text()
+    imageCache[imageFileName] = svgData;
 
     return svgData
 }
--- chessington/ui/static/scripts/main.js
+++ chessington/ui/static/scripts/main.js
@@ -1,8 +1,11 @@
 import { Board } from "./board.js"
 import { getBoardData } from "./client.js";
 import { SVG } from '@svgdotjs/svg.js'
+import { buildImageCache } from "./image_loader.js";
 
 async function main() {
+    await buildImageCache()
+
     const width = 400, height = 400
 
     const draw = SVG().addTo('body').size(width, height)
